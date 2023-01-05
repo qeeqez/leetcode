fdebed4ce0533463761e0f9fb8f72fa8466cd505
qeeqez <qeeqez@gmail.com>
2023-01-06 00:34:12 +0300

    Solve 2520. Count the Digits That Divide a Number

---
+++ src/test/java/com/qeeqez/easy/CountDigitsDivideNumber2520Test.java
@@ -0,0 +1,34 @@
+package com.qeeqez.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CountDigitsDivideNumber2520Test {
+
+    CountDigitsDivideNumber2520 solution = new CountDigitsDivideNumber2520();
+
+    @Test
+    public void testCase1() {
+        int num = 7;
+        int output = 1;
+
+        assertEquals(output, solution.countDigits(num));
+    }
+
+    @Test
+    public void testCase2() {
+        int num = 121;
+        int output = 2;
+
+        assertEquals(output, solution.countDigits(num));
+    }
+
+    @Test
+    public void testCase3() {
+        int num = 1248;
+        int output = 4;
+
+        assertEquals(output, solution.countDigits(num));
+    }
+}
