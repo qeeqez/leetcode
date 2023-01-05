4049520a6b891875ef91ca2d5d19abcb51023789
qeeqez <qeeqez@gmail.com>
2023-01-05 18:47:06 +0300

    Solve 1281. Subtract the Product and Sum of Digits of an Integer

---
+++ src/main/java/com/qeeqez/easy/SubstractProductAndSumOfDigitsInteger1281.java
@@ -0,0 +1,24 @@
+package com.qeeqez.easy;
+
+/**
+ * 1281. Subtract the Product and Sum of Digits of an Integer
+ * <p>
+ * Given an integer number n,
+ * return the difference between the product of its digits and the sum of its digits.
+ */
+public class SubstractProductAndSumOfDigitsInteger1281 {
+    public int subtractProductAndSum(int n) {
+        int prod = 1;
+        int sum = 0;
+
+        int digit;
+        while (n > 0) {
+            digit = n % 10;
+            n = n / 10;
+            prod *= digit;
+            sum += digit;
+        }
+        return prod - sum;
+    }
+}
+
