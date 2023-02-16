9a8e3deabf378a3697be5ba92f0491aacc1745cf
qeeqez <qeeqez@gmail.com>
2023-02-17 02:51:57 +0300

    Solve 231. Power of Two

--- README.md
+++ README.md
@@ -47,6 +47,7 @@
 [206. Reverse Linked List](./src/main/java/com/qeeqez/easy/ReverseLinkedList206.java)  
 [217. Contains Duplicate](./src/main/java/com/qeeqez/easy/ContainsDuplicate217.java)  
 [226. Invert Binary Tree](./src/main/java/com/qeeqez/easy/ValidAnagram242.java)  
+[231. Power of Two](./src/main/java/com/qeeqez/easy/PowerOfTwo231.java)  
 [232. Implement Queue using Stacks](./src/main/java/com/qeeqez/easy/ImplementQueueUsingStacks232.java)    
 [242. Valid Anagram](./src/main/java/com/qeeqez/easy/ValidAnagram242.java)  
 [270. Closest Binary Search Tree Value]   
---
+++ src/test/java/com/qeeqez/easy/PowerOfTwo231Test.java
@@ -0,0 +1,43 @@
+package com.qeeqez.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PowerOfTwo231Test {
+
+    PowerOfTwo231 solution = new PowerOfTwo231();
+
+    @Test
+    public void testCase1() {
+        int n = 1;
+
+        assertTrue(solution.isPowerOfTwoLoop(n));
+        assertTrue(solution.isPowerOfTwo(n));
+    }
+
+    @Test
+    public void testCase2() {
+        int n = 16;
+
+        assertTrue(solution.isPowerOfTwoLoop(n));
+        assertTrue(solution.isPowerOfTwo(n));
+    }
+
+    @Test
+    public void testCase3() {
+        int n = 3;
+
+        assertFalse(solution.isPowerOfTwoLoop(n));
+        assertFalse(solution.isPowerOfTwo(n));
+    }
+
+    @Test
+    public void testCase4() {
+        int n = -16;
+
+        assertFalse(solution.isPowerOfTwoLoop(n));
+        assertFalse(solution.isPowerOfTwo(n));
+    }
+}
