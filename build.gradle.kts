597be69d2596cd9383eb97ff2b72761ce452b1ee
qeeqez <qeeqez@gmail.com>
2023-08-23 19:18:29 +0300

    Bump junit from 5.9.3 to 5.10.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
 }
 
 tasks.withType<Test> {
